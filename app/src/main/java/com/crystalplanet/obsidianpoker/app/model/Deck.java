ed9ff00c6b735423c46d40d17a9af81e3726d535
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-02-27 01:34:07 +0100

    Added card deck model

---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/app/model/Deck.java
@@ -0,0 +1,27 @@
+package com.crystalplanet.obsidianpoker.app.model;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class Deck {
+
+    private Stack<Card> cards = new Stack<Card>();
+
+    public Deck() {
+        for (CardSuit suit : CardSuit.SUITS)
+            for (CardRank rank : CardRank.RANKS)
+                cards.push(new Card(suit, rank));
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        return cards.pop();
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+}
---
+++ app/src/test/java/com/crystalplanet/obsidianpoker/app/model/DeckTest.java
@@ -0,0 +1,45 @@
+package com.crystalplanet.obsidianpoker.app.model;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DeckTest extends TestCase {
+
+    public void testShuffle() {
+        ArrayList<Card> sortedDeck = new ArrayList<Card>();
+        ArrayList<Card> shuffledDeck = new ArrayList<Card>();
+
+        Deck d1 = new Deck();
+        Deck d2 = new Deck();
+
+        d2.shuffle();
+
+        while (!d1.isEmpty()) {
+            sortedDeck.add(d1.drawCard());
+            shuffledDeck.add(d2.drawCard());
+        }
+
+        Assert.assertFalse(compareOrder(sortedDeck, shuffledDeck));
+
+        Collections.sort(sortedDeck);
+        Collections.sort(shuffledDeck);
+
+        for (int i=0; i<52; ++i) {
+            Assert.assertEquals(0, sortedDeck.get(i).compareTo(shuffledDeck.get(i)));
+        }
+    }
+
+    private boolean compareOrder(ArrayList<Card> l1, ArrayList<Card> l2) {
+        int equal = 0;
+        for (int i=0; i<52; ++i) {
+            if (l1.get(i).compareTo(l2.get(i)) == 0) {
+                ++equal;
+            }
+        }
+
+        return equal > 2;
+    }
+}
