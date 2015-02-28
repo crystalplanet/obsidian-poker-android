ad42c1f100b5c77f765775db6747505f4b43237f
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-02-28 19:26:50 +0100

    Added a Pair utility class

---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/util/Pair.java
@@ -0,0 +1,12 @@
+package com.crystalplanet.obsidianpoker.util;
+
+public class Pair<T, U> {
+
+    public T first;
+    public U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+}
