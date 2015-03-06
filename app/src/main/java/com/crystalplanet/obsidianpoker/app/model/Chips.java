bcffa31c7bb0bbed0a1fa2294dac76f6c501a217
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-03-06 21:11:52 +0100

    Added Chips class

---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/app/model/Chips.java
@@ -0,0 +1,39 @@
+package com.crystalplanet.obsidianpoker.app.model;
+
+public class Chips implements Comparable<Chips> {
+
+    private int amount;
+
+    public Chips(int amount) {
+        this.amount = amount;
+    }
+
+    public Chips negate() {
+        return new Chips(-amount);
+    }
+
+    public Chips add(Chips other) {
+        return new Chips(amount + other.amount);
+    }
+
+    public Chips substract(Chips other) {
+        return add(other.negate());
+    }
+
+    @Override
+    public String toString() {
+        return (amount < 0 ? "- " : "") + "$" + Math.abs(amount);
+    }
+
+    @Override
+    public int compareTo(Chips other) {
+        return amount - other.amount;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || !(other instanceof Chips)) return false;
+        Chips otherChips = (Chips)other;
+        return amount == otherChips.amount;
+    }
+}
