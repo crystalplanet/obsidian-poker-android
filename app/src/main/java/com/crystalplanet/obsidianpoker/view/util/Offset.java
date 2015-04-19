933cd714ce687c9bd01792f8f1ea5072e75bd0de
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-04-19 13:36:24 +0200

    Added Offset class

---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/view/util/Offset.java
@@ -0,0 +1,22 @@
+package com.crystalplanet.obsidianpoker.view.util;
+
+public class Offset {
+
+    private int offsetTop;
+    private int offsetLeft;
+
+    public Offset(int width, int height, int defaultWidth, int defaultHeight) {
+        Scale scale = new Scale(width, height, defaultWidth, defaultHeight);
+
+        offsetTop = Math.round(scale.scale(((float) height - scale.scale(defaultHeight)) / 2));
+        offsetLeft = Math.round(scale.scale(((float) width - scale.scale(defaultWidth)) / 2));
+    }
+
+    public int offsetTop(int number) {
+        return number + offsetTop;
+    }
+
+    public int offsetLeft(int number) {
+        return number + offsetLeft;
+    }
+}
