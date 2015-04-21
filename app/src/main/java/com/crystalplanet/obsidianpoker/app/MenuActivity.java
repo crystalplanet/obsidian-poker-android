abb496e52803c80e5390f273c90f4811f8ed1365
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-04-21 22:37:09 +0200

    Added configuration for PokerGameView

--- app/src/main/java/com/crystalplanet/obsidianpoker/app/MenuActivity.java
+++ app/src/main/java/com/crystalplanet/obsidianpoker/app/MenuActivity.java
@@ -10,7 +10,6 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
-
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
