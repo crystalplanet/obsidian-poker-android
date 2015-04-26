61a7ac1b6a8373aea2e495a09a06eb25ffccb17c
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-04-26 12:38:26 +0200

    Added DrawableFactory & LayoutFactory stubs

---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/view/DrawableFactory.java
@@ -0,0 +1,12 @@
+package com.crystalplanet.obsidianpoker.view;
+
+import com.crystalplanet.obsidianpoker.view.drawable.Graphic;
+
+import java.util.Map;
+
+public class DrawableFactory {
+    public Drawable newDrawable(String name, Map<String, String> attr, Layout parent) {
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/view/LayoutFactory.java
@@ -0,0 +1,11 @@
+package com.crystalplanet.obsidianpoker.view;
+
+import java.util.Map;
+
+public class LayoutFactory extends DrawableFactory {
+
+    @Override
+    public Drawable newDrawable(String name, Map<String, String> attributes, Layout parent) {
+        return null;
+    }
+}
