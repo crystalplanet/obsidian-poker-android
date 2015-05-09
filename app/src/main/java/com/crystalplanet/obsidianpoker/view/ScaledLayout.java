d0a71422019941b5ef8dd31f4f144ddbc05b732a
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-05-10 01:28:29 +0200

    Changed the relative attribute of ScaledLayout to position

--- app/src/main/java/com/crystalplanet/obsidianpoker/view/ScaledLayout.java
+++ app/src/main/java/com/crystalplanet/obsidianpoker/view/ScaledLayout.java
@@ -5,10 +5,16 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 import com.crystalplanet.obsidianpoker.app.R;
-import com.crystalplanet.obsidianpoker.view.util.Offset;
-import com.crystalplanet.obsidianpoker.view.util.Scale;
+import com.crystalplanet.obsidianpoker.util.Offset;
+import com.crystalplanet.obsidianpoker.util.Scale;
 
 public class ScaledLayout extends ViewGroup implements Scalable {
+
+    public static final int ABSOLUTE = 2;
+
+    public static final int RELATIVE = 1;
+
+    public static final int BLOCK = 0;
 
     private Scale scale;
 
@@ -18,7 +24,7 @@ public class ScaledLayout extends ViewGroup implements Scalable {
 
     private int height;
 
-    private boolean relative;
+    private int position;
 
     public ScaledLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,7 +38,8 @@ public class ScaledLayout extends ViewGroup implements Scalable {
             );
             width = a.getInt(R.styleable.ScaledLayout_layout_width, 0);
             height = a.getInt(R.styleable.ScaledLayout_layout_height, 0);
-            relative = a.getBoolean(R.styleable.ScaledLayout_relative, false);
+
+            position = a.getInt(R.styleable.ScaledLayout_layout_position, BLOCK);
         } finally {
             a.recycle();
         }
@@ -69,7 +76,11 @@ public class ScaledLayout extends ViewGroup implements Scalable {
     }
 
     public boolean isRelative() {
-        return relative;
+        return position == RELATIVE;
+    }
+
+    public boolean isAbsolute() {
+        return position == ABSOLUTE;
     }
 
     @Override
@@ -86,8 +97,8 @@ public class ScaledLayout extends ViewGroup implements Scalable {
         child.layout(
             scale(child.getOffsetLeft()) - (isRelative() ? 0 : l),
             scale(child.getOffsetTop()) - (isRelative() ? 0 : t),
-            scale(child.getOffsetLeft()) - (isRelative() ? 0 : l) + scale(child.scaledWidth()),
-            scale(child.getOffsetTop()) - (isRelative() ? 0 : l) + scale(child.scaledHeight())
+            scale(child.getOffsetLeft() + child.scaledWidth()) - (isRelative() ? 0 : l),
+            scale(child.getOffsetTop() + child.scaledHeight()) - (isRelative() ? 0 : t)
         );
     }
 }
