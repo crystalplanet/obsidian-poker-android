15f84984466976d6cd09f2ed295ac35fdca5262b
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-05-09 23:36:47 +0200

    Added GenericScaledLayout

---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/view/GenericScaledLayout.java
@@ -0,0 +1,19 @@
+package com.crystalplanet.obsidianpoker.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import com.crystalplanet.obsidianpoker.view.util.Scale;
+
+public class GenericScaledLayout extends ScaledLayout {
+
+    public GenericScaledLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+
+        setScale(new Scale(w, h, scaledWidth(), scaledHeight()));
+    }
+}
