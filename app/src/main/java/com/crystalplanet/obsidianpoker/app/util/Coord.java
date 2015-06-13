e96d9aee57d359a2c53d9305ec9bfb4ea3914cdf
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-06-13 10:32:06 +0200

    Added a Coord util class

---
+++ app/src/main/java/com/crystalplanet/obsidianpoker/app/util/Coord.java
@@ -0,0 +1,13 @@
+package com.crystalplanet.obsidianpoker.app.util;
+
+public class Coord {
+
+    public float x;
+
+    public float y;
+
+    public Coord(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
